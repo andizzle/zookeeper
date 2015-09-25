97c28baae7703997a3cc409672d3bef7b048aead
Andy Zhang <andizzle.zhang@gmail.com>
2015-09-25 12:27:20 +1000

    auto generate MYID if none given. Zookeeper can be start by `init` now.

--- zk-init.sh
+++ zk-init.sh
@@ -2,38 +2,73 @@
 
 set -e
 
-MYID=$1
 ZK=$2
+MYID=$3
 
 HOSTNAME=`hostname`
 IPADDRESS=`/sbin/ip route|awk '/eth1/ { print $9 }'`
 
 RE="^[0-9]+$"
 
+# config single or cluster
 function config {
     if [ -n "$ZK" ]; then
         # join an existing cluster
+
         echo "`bin/zkCli.sh -server $ZK:2181 get /zookeeper/config|grep ^server`" >> conf/zoo.cfg.dynamic
+
+	# if no MYID specified, pick one
+        if ! [[ $MYID =~ $RE ]]; then
+            # get all participants' id
+            ids=$(cat conf/zoo.cfg.dynamic | grep -Eo '[0-9]{1,3}=' | awk -F'=' '{print $1}')
+            MYID=$(next_id $ids)
+        fi
+
         echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> conf/zoo.cfg.dynamic
         cp conf/zoo.cfg.dynamic conf/zoo.cfg.dynamic.org
     else
         # start a new cluster
+        if ! [[ $MYID =~ $RE ]]; then
+            MYID=1
+        fi
         echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> conf/zoo.cfg.dynamic
     fi
-
     bin/zkServer-initialize.sh --configfile=/opt/zookeeper/conf/zoo.cfg --force --myid=$MYID
 }
 
+# config follower when join a cluster
 function config_follower {
     if [ -n "$ZK" ]; then
         ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' bin/zkServer.sh start
         bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
         bin/zkServer.sh stop
+	echo $MYID
     fi
 }
 
+# id generate
+function next_id {
+    next_id=1
+    current_id=1
+    ids=$1
+
+    while [ "$current_id" -lt 256 ]; do
+        for id in $ids; do
+            if [ "$next_id" -eq "$id" ]; then
+		let "next_id++"
+		current_id=$next_id
+                continue
+            else
+		current_id=256
+                break
+            fi
+        done
+    done
+    echo $next_id
+}
+
 # start the zookeeper if the param is an id
-if [[ $MYID =~ $RE ]]; then
+if ! [ -n "$1" ] || [ "$1" = 'init' ]; then
 
     cd /opt/zookeeper
 
